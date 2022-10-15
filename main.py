72d2e4316d492aa31d0edaf7aefabc68604d8c35
yourjss <115801053+yourjss@users.noreply.github.com>
2022-10-14 23:22:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os, sys, re, glob, json, zipfile, uuid
 
-UUID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"  # uuid.uuid4().__str__()
+UUID = "38ae9a72-7bd1-4b44-bade-a8cea313f8c6"  # uuid.uuid4().__str__()
 ProxySite = "www.python.org"
 Port = 8080
 
